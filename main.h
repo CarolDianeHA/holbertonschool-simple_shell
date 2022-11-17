ea5c38d1c0464a962b7ffde8d85f2102564b5aff
Melady1980 <carol.d.hernandez@outlook.com>
2022-11-17 00:55:49 +0000

    Header file.

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+
+#endif
