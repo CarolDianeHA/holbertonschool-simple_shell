538d99bca747019ba23ca591619ed2039bc07b8a
josemls <jose_9513@hotmail.com>
2022-11-18 17:53:13 +0000

    adding function that splits into tokens

---
+++ split_line.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+*split_line- split string in to tokens
+*lineptr: input
+*Return: str, needs to be free
+*/
+
+char **split_line(char *lineptr)
+{
+	int num = 0;
+	int capacity =  25;
+	char **str = malloc(capacity * sizeof(char *));
+
+	char *delim = " \n\t\r:";
+
+	str[num] = strtok(lineptr, delim);
+
+	while (str[num] != NULL)
+	{
+		num++;
+		str[num] = strtok(NULL, delim);
+	}
+
+	return (str);
+}
+
+/*
+ * int main (void)
+{
+	char lineptr[] = "/bin/ls -l two:t three \n";
+	char **str;
+	str = split_line(lineptr);
+	int n = 0;
+	while ( str[n] != NULL)
+	{
+	printf("str [%d] = %s\n", n, str[n]);
+	n++;
+	}
+	return (1);
+}
+*/
