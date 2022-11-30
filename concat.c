045c918de420e871fc30fb5c4bf2cb6eeb3b234e
josemls <jose_9513@hotmail.com>
2022-11-30 17:25:30 +0000

    adding concat function

---
+++ concat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * concat_str_dir - concat token and value
+ * @array: token
+ * @val: value
+ * Return: new string
+ */
+
+char *concat_str_dir(char *array, char *command)
+{
+	char *new;
+	char *final;
+
+	new = strcat(array, "/");
+	final = strcat(new, command);
+
+	return (final);
+}
