d95d72104d56146633cf60e2b16c6c905aeee9a8
josemls <jose_9513@hotmail.com>
2022-11-30 17:30:02 +0000

    fixing return in tokenizer

--- split_line.c
+++ split_line.c
@@ -11,7 +11,6 @@ char **split_line(char *lineptr)
 	int num = 0;
 	int capacity =  25;
 	char **str = malloc(capacity * sizeof(char *));
-
 	char *delim = " \n\t\r:";
 
 	str[num] = strtok(lineptr, delim);
@@ -22,21 +21,6 @@ char **split_line(char *lineptr)
 		str[num] = strtok(NULL, delim);
 	}
 
-	return (str);
+	return(str);
 }
 
-/*
- * int main (void)
-{
-	char lineptr[] = "/bin/ls -l two:t three \n";
-	char **str;
-	str = split_line(lineptr);
-	int n = 0;
-	while ( str[n] != NULL)
-	{
-	printf("str [%d] = %s\n", n, str[n]);
-	n++;
-	}
-	return (1);
-}
-*/
